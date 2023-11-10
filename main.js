7451cbbbdb90871818d025efb5c7958e6cbf78c5
Shravan-devi <83762735+Shravan-devi@users.noreply.github.com>
2023-11-10 19:35:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 statuss="";
+objects=[];
 function preload(){
     video=createVideo("video.mp4");
     video.hide();
@@ -12,6 +13,30 @@ function setup(){
 
 function draw(){
     image(video,0,0,480,380);
+    if(statuss != ""){
+        object_detector.detect(video, gotResults);
+        document.getElementById("status").innerHTML="Status:Objects Detected";
+        document.getElementById("no_objects").innerHTML="Number of objects detected: " + objects.length;
+        for(var i=0; i<objects.length; i++){
+            fill("#FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+ " " + percent + "%",objects[i].x + 15, objects[i].y + 15);
+            noFill()
+            stroke("#FF0000")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            
+        }
+    }
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
 }
 
 function start(){
